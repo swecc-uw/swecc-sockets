34fe90b1dce7deeafd8ebdff4e02aaedf58f28ea
Elijah Melton <elimelt@uw.edu>
2025-03-23 01:58:30 -0700

    add group based perms

--- app/auth.py
+++ app/auth.py
@@ -9,6 +9,7 @@ from .config import settings
 class TokenPayload(BaseModel):
     user_id: int
     username: str
+    groups: list[str] = []
     exp: datetime
 
 
@@ -24,14 +25,20 @@ class Auth:
             if token_data.exp < datetime.now(timezone.utc):
                 return None
 
-            return {"user_id": token_data.user_id, "username": token_data.username}
+            return {
+                "user_id": token_data.user_id,
+                "username": token_data.username,
+                "groups": token_data.groups,
+            }
         except (JWTError, ValidationError):
             return None
 
     @staticmethod
-    async def authenticate_ws(websocket: WebSocket, token: str) -> Optional[dict]:
+    async def authenticate_ws(
+        websocket: WebSocket, token: str, required_groups: list[str] = ["is_verified"]
+    ) -> Optional[dict]:
         user = await Auth.validate_token(token)
-        if not user:
+        if not user or not all(group in user["groups"] for group in required_groups):
             await websocket.close(code=status.WS_1008_POLICY_VIOLATION)
             return None
         return user
